7f164b545f95079ed009c7526aeea52074ba745c
Ryan Dawson <ryan.dawson@alfresco.com>
2018-05-04 10:46:46 +0100

    port from incubator

--- activiti-cloud-services-application/activiti-cloud-services-app-api/src/main/java/org/activiti/cloud/app/api/model/ActivitiCloudAppDeployer.java
+++
@@ -1,8 +0,0 @@
-package org.activiti.cloud.app.api.model;
-
-public interface ActivitiCloudAppDeployer {
-
-    String deploy(BuildingBlock bb);
-
-    String getStatus(String provisionId);
-}
--- activiti-cloud-services-application/activiti-cloud-services-app-api/src/main/java/org/activiti/cloud/app/api/model/ServiceDesc.java
+++
@@ -1,5 +0,0 @@
-package org.activiti.cloud.app.api.model;
-
-public interface ServiceDesc extends BuildingBlock {
-
-}
